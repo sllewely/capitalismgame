b3e703ac8c26af4452df111bef8950a4095d651b
Sarah Llewelyn <se.llewelyn@gmail.com>
2024-01-24 21:18:49 -0500

    delete code in player controller unused

--- Assets/Scripts/Dungeon/PlayerController.cs
+++ Assets/Scripts/Dungeon/PlayerController.cs
@@ -53,72 +53,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // // Movement
-        // float x_axis = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        // float y_axis = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        //
-        // if (Mathf.Abs(x_axis) > Mathf.Abs(y_axis))
-        // {
-        //     if (x_axis > 0.0)
-        //     {
-        //         transform.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
-        //     }
-        //     else if (x_axis < 0.0)
-        //     {
-        //         transform.eulerAngles = new Vector3(0.0f, 270.0f, 0.0f);
-        //     }
-        // }
-        // else if (Mathf.Abs(y_axis) > Mathf.Abs(x_axis))
-        // {
-        //     if (y_axis > 0.0)
-        //     {
-        //         transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
-        //     }
-        //     else if (y_axis < 0.0)
-        //     {
-        //         transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
-        //     }
-        // }
-        
-    
         // Attack
         if (Input.GetKeyDown(KeyCode.J)) {
             weaponAnimator.Play("Base Layer.Attack1");
         }
 
+        // Rotation
         var targetAngle = Mathf.Atan2(directionInput.x, directionInput.y) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
         transform.rotation = Quaternion.Euler(0, angle, 0);
         
-        Debug.Log("move is " + (speed * new Vector3(moveInput.x, 0.0f, moveInput.y)));
+        // Move
         characterController.Move(speed * new Vector3(moveInput.x, 0.0f, moveInput.y) * Time.deltaTime);
     }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
-    // // When game object is enabled, so is the input, else disabled.
-    // private void OnEnable()
-    // {
-    //     input.Enable();
-    //     input.Player.Movement.performed += OnMovementPerformed;
-    //     input.Player.Movement.performed += OnMovementCancelled;
-    // }
-    //
-    // private void OnDisable()
-    // {
-    //     input.Disable();
-    //     input.Player.Movement.performed -= OnMovementPerformed;
-    //     input.Player.Movement.performed -= OnMovementCancelled;
-    // }
 
     private void OnMovementPerformed(InputAction.CallbackContext value)
     {
         moveInput = value.ReadValue<Vector2>();
-        // Debug.Log("move value " + moveInput);
-        // direction = new Vector3()
     }
 
     private void OnMovementCancelled(InputAction.CallbackContext value)
