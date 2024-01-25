7ef1dc02122bdf36573d84f6c0b1e384a9b6e3ef
Michail Denchev <mdenchev@gmail.com>
2024-01-24 22:50:54 -0500

    Maybe fix input
    Wasn't working and not it is, maybe this is needed maybe it's not, who knows, life is full of mysteries

--- Assets/Scripts/Dungeon/PlayerController.cs
+++ Assets/Scripts/Dungeon/PlayerController.cs
@@ -37,6 +37,7 @@ public class PlayerController : MonoBehaviour
         weaponAnimator = weapon.GetComponent<Animator>();
         input.Player.Movement.performed += moving =>
         {
+            Debug.Log("move");
             moveInput = moving.ReadValue<Vector2>();
             directionInput = moving.ReadValue<Vector2>();
         };
