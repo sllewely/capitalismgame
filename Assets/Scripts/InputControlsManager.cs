51b7059f623f8879a692cfc9afb56659a39c06c4
Sarah Llewelyn <se.llewelyn@gmail.com>
2024-01-27 18:57:28 -0500

    open and close inventory

--- Assets/Scripts/InputControlsManager.cs
+++ Assets/Scripts/InputControlsManager.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ButtonControlsManager : MonoBehaviour
+using UnityEngine.InputSystem;
+public class InputControlsManager : MonoBehaviour
 {
+    
+    private InputControls input = null;
+
+    public GameObject inventoryUI;
+    
+    private void Awake()
+    {
+        input = new InputControls();
+        input.Enable();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
+        if (inventoryUI == null)
+        {
+            Debug.LogError("InputControlsManager missing inventoryUI");
+        }
+        input.Player.OpenInventory.performed += pressed =>
+        {
+            // Toggle Inventory open/close
+            var isInventoryOpen = inventoryUI.activeSelf;
+            inventoryUI.SetActive(!isInventoryOpen);
+        };
         
     }
 
